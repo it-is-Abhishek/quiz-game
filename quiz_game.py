c4b49481cc816e3be5f459c4627ba0ad397a2779
it-is-Abhishek <abhishekverma1489@gmail.com>
2025-06-09 16:06:58 +0530

    display message

--- quiz_game.py
+++ quiz_game.py
@@ -10,3 +10,18 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+import time
+
+def display_welcome():
+    print("\n"*50)
+    print("****************************************************")
+    print("*                                                   *")
+    print("*           WELCOME TO THE QUIZ GAME!              *")
+    print("*                                                   *")
+    print("****************************************************")
+    print("\n")
+
+    print("test your game knowledge and win!")
+    print("here's how to play:")
+    print()
