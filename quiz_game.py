5ce997ebd51c7f855aa5443c45e897f96f117420
it-is-Abhishek <abhishekverma1489@gmail.com>
2025-06-09 16:00:54 +0530

    first commit

---
+++ quiz_game.py
@@ -0,0 +1,12 @@
+"""
+Quiz Game Project
+-----------------
+A simple interactive quiz game with multiple question types and scoring system.
+"""
+
+def main():
+    print("Welcome to my Quiz Game!")
+    print("------------------------")
+
+if __name__ == "__main__":
+    main()
