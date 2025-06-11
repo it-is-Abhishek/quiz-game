ed02aa0095646214c8b0cb3e79529f4f7db94e34
it-is-Abhishek <abhishekverma1489@gmail.com>
2025-06-11 09:01:04 +0530

    Aded questions

--- quiz_game.py
+++ quiz_game.py
@@ -37,3 +37,52 @@ def display_welcome():
     print("\n"*50)
 
 display_welcome()
+
+
+play_game = True
+while play_game:
+    current_score  = 0
+    current_question = 0
+
+    for question in questions:
+        display_question(question)
+
+        user_ans = input("Please enter your answer (or type 'quit' to exit): ")
+        if user_ans.lower() == "quit":
+            play_game = False
+            break
+
+        current_score, feedback = check_ans(question, username, current_score)
+
+        print(feedback)
+        current_question += 1
+    
+    if play_game:
+        print(f"\nRound completed! Your final score is {current_score}")
+        play_again = input("would you like to play again? (yes/no):")
+        
+        if play_game.lower() != "yes":
+            play_game = Flase
+
+print("Thankyou for playing!")
+
+
+score = 0
+current_question = 0
+total_question = len(questions)
+
+
+questions = [
+    {
+        "question": "What is the capital of France?",
+        "options": ["Paris", "London", "Berlin", "Madrid"],
+        "correct": 0
+    },
+    {
+        "question": "Which planet is known as the Red Planet?",
+        "options": ["Earth", "Mars", "Venus", "Saturn"],
+        "correct": 1
+    }
+]
+
+while game_activities():
